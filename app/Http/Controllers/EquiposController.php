88cd6d43e410cf134844b480984f33d1c7166750
Cristian Solano <cfsv2018@gmail.com>
2026-01-22 09:23:39 -0500

    Fix case sensitivity in import statement for Municipios

--- app/Http/Controllers/EquiposController.php
+++ app/Http/Controllers/EquiposController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Equipos;
-use App\Models\municipios;
+use App\Models\Municipios;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
